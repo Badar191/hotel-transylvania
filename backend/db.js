c0af8727659e5a8bcfa8397bc9e9490565fa471e
Badar <abdulrehmanbadar191@gmail.com>
2023-06-28 17:59:31 +0500

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ let ConnectionDb;
 
 module.exports = {
   connectToDb: (cb) => {
-    MongoClient.connect('mongodb+srv://badar191:sUWq8a3Ziqyvhhow@cluster0.0thtj3l.mongodb.net/')
+    MongoClient.connect('mongodb+srv://badar191:sUWq8aefghi3Ziqyvhhow@cluster0.0thtj3l.mongodb.net/')
       .then(client => {
         console.log('helllloooooooooo')
         ConnectionDb = client.db();
